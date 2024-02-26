1de6f8876e2a7b83f5ec7d4fe62feaf0f86d4c3b
yozen0405 <yozen0405@gmail.com>
2024-02-26 23:17:41 +0800

    improving

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import Header from '../components/Header';
 import './About.css';
 
 const About = () => {
+    const handleGitHubClick = () => {
+        window.open('https://github.com/yozen0405/equation-solver', '_blank');
+    };
+
     return (
         <div className="about-page">
             <Header />
             <div className="content">
-                <h2>About Sir Isaac Newton</h2>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/GodfreyKneller-IsaacNewton-1689.jpg/250px-GodfreyKneller-IsaacNewton-1689.jpg" alt="Sir Isaac Newton" className="image" />
-                <p>Sir Isaac Newton (1643–1727) was an English mathematician, physicist, astronomer, and author who is widely recognised as one of the most influential scientists of all time and as a key figure in the scientific revolution.</p>
-                <p>His book <i>Philosophiæ Naturalis Principia Mathematica</i>, first published in 1687, laid the foundations of classical mechanics and is considered one of the most important scientific works ever written.</p>
-                <p>Newton made groundbreaking contributions to optics, mathematics (especially calculus), and celestial mechanics. He formulated the laws of motion and universal gravitation, which dominated scientists' view of the physical universe for the next three centuries.</p>
-                <p>Newton's work provided the Enlightenment-era mathematicians and philosophers with a coherent model of the physical universe, allowing them to develop a mechanistic view of nature.</p>
-                <p>Newton's discoveries and theories laid the groundwork for many scientific advances that followed, making him one of the most influential scientists in history.</p>
-                <p>To learn more about Sir Isaac Newton, you can explore various biographies, documentaries, and academic publications dedicated to his life and work.</p>
+                <h2>About The Website</h2>
+                <button className="github-button" onClick={handleGitHubClick}>
+                    <FontAwesomeIcon icon={faGithub} style={{ marginRight: '0.5em' }} /> View GitHub Repo
+                </button>
             </div>
         </div>
     );
--- src/pages/QuadraticEquationSolver.js
+++ src/pages/QuadraticEquationSolver.js
@@ -1,5 +1,5 @@
 import Header from "../components/Header";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './LinearEquationSolver.css';
 
 function QuadraticEquationSolver() {
@@ -7,6 +7,65 @@ function QuadraticEquationSolver() {
   const [b, setB] = useState('');
   const [c, setC] = useState('');
   const [result, setResult] = useState({ x1: '', x2: '' });
+  const [twoA, setTwoA] = useState('');
+  const [negativeB, setNegativeB] = useState('');
+  const [Discriminant, setDiscriminant] = useState('');
+  const [sqrtDiscriminant, setSqrtDiscriminant] = useState('');
+  const [positiveFactor0, setPositiveFactor0] = useState('');
+  const [positiveFactor1, setPositiveFactor1] = useState('');
+  const [negativeFactor0, setNegativeFactor0] = useState('');
+  const [negativeFactor1, setNegativeFactor1] = useState('');
+  const [tmp, setTmp] = useState('');
+  const [tmp2, setTmp2] = useState('');
+
+  const findBiggestFactor = (num) => {
+    let biggestFactor = 1;
+    for (let i = 2; i * i <= num; i++) {
+      if (num % (i * i) === 0) {
+        biggestFactor = Math.max(biggestFactor, i);
+      }
+    }
+    return [biggestFactor, num / (biggestFactor * biggestFactor)];
+  };
+
+  const findBiggestGcdForPairs = (a, b) => {
+    const newA = Math.abs(a);
+    const newB = Math.abs(b);
+    let biggestGcd = 1;
+    for (let i = 2; i <= newA && i <= newB; i++) {
+      if ((newA % i === 0) && (newB % i === 0)) {
+        biggestGcd = Math.max(biggestGcd, i);
+      }
+    }
+    a /= biggestGcd;
+    b /= biggestGcd;
+    return [a, b];
+  }; 
+
+  const findBiggestGcd = (a, b, c) => {
+    const newA = Math.abs(a);
+    const newB = Math.abs(b);
+    const newC = Math.abs(c);
+    let biggestGcd = 1;
+    for (let i = 2; i <= newA && i <= newB && i <= newC; i++) {
+      if ((newA % i === 0) && (newB % i === 0) && (newC % i === 0)) {
+        biggestGcd = Math.max(biggestGcd, i);
+      }
+    }
+    a /= biggestGcd;
+    b /= biggestGcd;
+    c /= biggestGcd;
+    return [a, b, c];
+  };
+
+  const solveLinearEquation = () => {
+    setTwoA(b);
+    setNegativeB(-c);
+    setDiscriminant(0);
+    setSqrtDiscriminant(0);
+    const x = - c / b;
+    setResult({ x1: x, x2: '' });
+  };
 
   const handleInputChange = (e, setter) => {
     setter(e.target.value);
@@ -16,16 +75,43 @@ function QuadraticEquationSolver() {
     e.preventDefault();
 
     const discriminant = b * b - 4 * a * c;
+    const numB = (a < 0) ? -parseInt(b) : parseInt(b);
+    const numC = (a < 0) ? -parseInt(c) : parseInt(c);
+    const numA = (a < 0) ? -parseInt(a) : parseInt(a);
+
+    if (numA === 0) {
+      solveLinearEquation();
+      return;
+    }
 
     if (discriminant > 0) {
-      const x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-      const x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+      const x1 = (-numB + Math.sqrt(discriminant)) / (2 * numA);
+      const x2 = (-numB - Math.sqrt(discriminant)) / (2 * numA);
       setResult({ x1, x2 });
     } else if (discriminant === 0) {
-      const x = -b / (2 * a);
-      setResult({ x, x });
+      const x = -numB / (2 * numA);
+      setResult({ x });
     } else {
       setResult('No real roots');
+      return;
+    }
+    const [outD, sqrtD] = findBiggestFactor(discriminant);
+    const [twoTimesA, negativeBVal, newoutD] = findBiggestGcd(2 * numA, -numB, outD);
+
+    const [newp0, newp1] = findBiggestGcdForPairs(negativeBVal + newoutD, twoTimesA);
+    setPositiveFactor0(newp0);
+    setPositiveFactor1(newp1);
+    const [newn0, newn1] = findBiggestGcdForPairs(negativeBVal - newoutD, twoTimesA);
+    setNegativeFactor0(newn0);
+    setNegativeFactor1(newn1);
+
+    setTwoA(twoTimesA);
+    setNegativeB(negativeBVal);
+    setDiscriminant(newoutD);
+    setSqrtDiscriminant(sqrtD);
+    if (parseInt(sqrtD) === 0 || parseInt(newoutD) === 0) {
+      setSqrtDiscriminant(0);
+      setDiscriminant(0);
     }
   };
 
@@ -67,8 +153,11 @@ function QuadraticEquationSolver() {
         {result && typeof result === 'object' ? (
           <div className="solution">
             <h3>Solution:</h3>
-            <p>x1 = {result.x1}</p>
-            <p>x2 = {result.x2}</p>
+            {result.x2 ? (
+              <p>x1 = {result.x1},  x2 = {result.x2}</p>
+            ) : (
+              <p>x = {result.x1}</p>
+            )}
           </div>
         ) : (
           <div className="solution">
@@ -76,6 +165,81 @@ function QuadraticEquationSolver() {
             <p>{result}</p>
           </div>
         )}
+        {result && typeof result === 'object' ? (
+          <div className="intermediate-values">
+            <h3>Intermediate Values:</h3>
+            <p>Solution: &nbsp;
+              {sqrtDiscriminant === 1 ? (
+                <span>
+                <div style={{ display: 'inline-block' }}>
+                  {positiveFactor1 === 1 || positiveFactor1 === 0 ?
+                    <p>{positiveFactor0}</p> : (
+                      <div class="fraction">
+                        <span class="fup">
+                          {positiveFactor0}
+                        </span>
+                        <span class="fdn">
+                          {positiveFactor1}
+                        </span>
+                      </div>
+                  )}
+                </div>
+                <div style={{ display: 'inline-block' }}>
+                  <p>,</p>
+                </div>
+                <div style={{ display: 'inline-block' }}>
+                  {negativeFactor1 === 1 || negativeFactor1 === 0 ?
+                    <p>{negativeFactor0}</p> : (
+                      <div class="fraction">
+                        <span class="fup">
+                          {negativeFactor0}
+                        </span>
+                        <span class="fdn">
+                          {negativeFactor1}
+                        </span>
+                      </div>
+                  )}
+                </div>
+              </span>              
+              ) : (
+                <div class="fraction">
+                  {Discriminant !== 0 && sqrtDiscriminant !== 0 ? (
+                    <span class="fup">
+                      {negativeB === 0 ?
+                        null :
+                        <span>{negativeB}</span>
+                      }
+                      &nbsp;±&nbsp;
+                      {Discriminant === 1 ?
+                        null :
+                        <span>{Discriminant}</span>
+                      }
+                      {sqrtDiscriminant === 1 ?
+                        null : (
+                          <div className="sqrt">
+                            &radic;
+                            <span className="overline">
+                              &nbsp;{sqrtDiscriminant}&nbsp;
+                            </span>
+                          </div>
+                        )}
+                    </span>
+                  ) : (
+                    <span class="fup">
+                      {negativeB}
+                    </span>
+                  )}
+                  {parseInt(twoA) !== 1 ?
+                    <span className="bar">/</span> : null
+                  }
+                  {parseInt(twoA) !== 1 ?
+                    <span className="fdn">{twoA}</span> : null
+                  }
+                </div>
+              )}
+            </p>
+          </div>) : null
+        }
       </div>
     </>
   );
